91bd00dbc351207abd2fcd203f39a32088d551f3
Ignacio Garcia <nachog.2000@hotmail.com>
2020-06-09 20:37:26 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,38 @@ module Lib where
 import Text.Show.Functions
 
 laVerdad = True
+
+
+data Gimnasta = UnGimnasta {
+    edad :: Float,
+    peso :: Float,
+    tonificacion :: Float
+}deriving (Show)
+
+pancho :: Gimnasta
+pancho = UnGimnasta {
+    edad = 40,
+    peso = 120,
+    tonificacion = 1
+}
+
+andres :: Gimnasta
+andres = UnGimnasta {
+    edad = 22,
+    peso = 80,
+    tonificacion = 6
+}
+
+relax :: Float -> Gimnasta -> Gimnasta
+relax minutos persona = persona
+
+-- Punto 1)
+
+estaObeso :: Gimnasta -> Bool
+estaObeso gimnasta = peso gimnasta > 100
+
+estaSaludable :: Gimnasta -> Bool
+estaSaludable gimnasta = (not.estaObeso) gimnasta && tonificacion gimnasta > 5
+
+
+
