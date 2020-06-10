8ae14dca367b3e4636dc4c5c4e2cf3415fd096ae
Ignacio Garcia <nachog.2000@hotmail.com>
2020-06-09 21:32:57 -0300

    Punto 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -45,3 +45,43 @@ quemarCalorias gimnasta calorias | estaObeso gimnasta = gimnasta {peso = bajarPe
 
 bajarPeso :: Gimnasta -> Float -> Float
 bajarPeso gimnasta cantidad = peso gimnasta - cantidad
+
+
+-- Punto 3)
+
+type Ejercicio = Minuto -> Gimnasta -> Gimnasta
+type Minuto = Float
+
+caminata :: Ejercicio
+caminata minutos gimnasta = quemarCalorias gimnasta (minutos * 5)
+
+entrenamientoEnCinta :: Ejercicio
+entrenamientoEnCinta minutos gimnasta = quemarCalorias gimnasta (velocidadPromedio minutos * minutos)
+
+velocidadPromedio :: Minuto -> Float
+velocidadPromedio tiempo = (velocidadInicial + velocidadFinal tiempo) / 2
+
+velocidadInicial :: Float
+velocidadInicial = 6
+
+velocidadFinal :: Minuto->Float
+velocidadFinal tiempo = velocidadInicial + (tiempo/5)
+
+pesas :: Float ->  Ejercicio
+pesas kilos minutos gimnasta | minutos > 10 = tonificar (kilos / 10) gimnasta
+                             | otherwise = gimnasta
+
+tonificar :: Float -> Gimnasta -> Gimnasta
+tonificar cantidad gimnasta = gimnasta {tonificacion = tonificacion gimnasta + cantidad}
+
+colina :: Float -> Ejercicio
+colina inclinacion minutos gimnasta = quemarCalorias gimnasta (2 * minutos * inclinacion)
+
+montaña :: Float -> Ejercicio
+montaña inclinacion minutos = (tonificar 1). (primeraColina inclinacion minutos). (segundaColina inclinacion minutos)
+
+primeraColina :: Float -> Ejercicio
+primeraColina inclinacion minutos = colina inclinacion (minutos / 2)
+
+segundaColina :: Float -> Ejercicio
+segundaColina inclinacion minutos = colina (inclinacion + 3) (minutos / 2)
